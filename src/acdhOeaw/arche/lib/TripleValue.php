62c3f5296cc325de9a35121a08f03c5ebeb99305
Mateusz Żółtak <zozlak@zozlak.org>
2023-03-15 12:26:28 +0100

    TripleValue class added

---
+++ src/acdhOeaw/arche/lib/TripleValue.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2023 Austrian Centre for Digital Humanities.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace acdhOeaw\arche\lib;
+
+use zozlak\RdfConstants as RDF;
+
+/**
+ * Simole container for an RDF triple value read from a database
+ *
+ * @author zozlak
+ */
+class TripleValue {
+
+    static public function fromDbRow(object $row) {
+        $triple        = new Triple();
+        $triple->type  = $row->type;
+        $triple->lang  = (string) $row->lang;
+        $triple->value = $row->value_t ?? $row->value_n ?? $row->value;
+        return $triple;
+    }
+
+    public string $type;
+    public string $lang;
+    public string | int | float | bool $value;
+}
